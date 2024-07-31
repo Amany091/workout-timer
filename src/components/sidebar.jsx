3916e5d86365c3e39709432e9659b839cc714333
amany091 <2manym7md90@gmail.com>
2024-07-31 13:48:23 +0300

    fix:functionality editing

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { tasks } from './tasks'
 
 const Sidebar = ({ setTask, active,setActive}) => {
-    const time = tasks.map(({ time }) => time)
+    const workoutSeconds = [...tasks].map(({ time }) => time).reduce((acc,curr)=> acc+curr)
     return (
         <div>
-            <h5 className="text-center">{Math.floor(time.reduce((acc, curr) => acc + curr) / 60)} Minutes Workout </h5>
-            {tasks.map((task, index) => {
+            <h5 className="text-center">{Math.floor(workoutSeconds /60)} Minutes Workout and {workoutSeconds %60} seconds </h5>
+            {[...tasks].map((task, index) => {
                 return (
                     <div
                         className={`task d-flex align-items-baseline ${active.id === task.id ? 'bg-success': ""} px-1  `}
@@ -15,7 +15,7 @@ const Sidebar = ({ setTask, active,setActive}) => {
                             setTask(index)
                         }} key={task.id} >
                         <h5 className='me-3' >{task.task}</h5>
-                        <p className='me-2' >{task.time}</p>
+                        <p className='me-2' >{task.time}s</p>
                         <p>{task.type}</p>
                     </div>
                 )
